9f05922d6388dff816d8e743fd8785d08a017c9e
pirus99 <Miles.Pickull@web.de>
2026-03-03 01:01:52 +0100

    Rename CREATE_QUIZ_URL to createQuiz

--- shared/js/config.js
+++ shared/js/config.js
@@ -4,5 +4,5 @@ const LOGIN_URL = "login/";
 const REGISTER_URL = "register/";
 const LOGOUT_URL = "logout/";
 const TOKENREFRESH_URL = "token/refresh/";
-const CREATE_QUIZ_URL = "quizzes/";
+const CREATE_QUIZ_URL = "createQuiz/";
 const GET_QUIZ_URL = "quizzes/";
